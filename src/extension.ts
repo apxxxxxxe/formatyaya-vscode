a405dcdd183e34bcf8afdc0325a8239aa6b4bf9f
apxxxxxxe <calcium629@gmail.com>
2022-12-11 19:45:30 +0900

    update: OSを判別,実行ファイルを使い分けるように

--- src/extension.ts
+++ src/extension.ts
@@ -1,57 +1,118 @@
-import * as vscode from 'vscode';
-import { execFileSync } from 'child_process';
-import { fileSync} from 'tmp';
-import { writeFileSync } from 'fs';
+import * as vscode from "vscode";
+import { execFileSync } from "child_process";
+import { fileSync } from "tmp";
+import { writeFileSync } from "fs";
+import { platform, arch } from "process";
+
+const isNotSupported = "ISNOTSUPPORTED";
 
 export function activate(context: vscode.ExtensionContext) {
-	const provideDocumentFormattingEdits = <vscode.DocumentFormattingEditProvider>{
-		provideDocumentFormattingEdits: (document: vscode.TextDocument) => format(document, null)
-	};
-	context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('aya', provideDocumentFormattingEdits));
+  const provideDocumentFormattingEdits = <
+    vscode.DocumentFormattingEditProvider
+  >{
+    provideDocumentFormattingEdits: (document: vscode.TextDocument) =>
+      format(document, null),
+  };
+  context.subscriptions.push(
+    vscode.languages.registerDocumentFormattingEditProvider(
+      "aya",
+      provideDocumentFormattingEdits
+    )
+  );
 }
 
-export function format(document: vscode.TextDocument,range: vscode.Range | null) {
-	if (range === null) {
-        range = initDocumentRange(document);
+export function format(
+  document: vscode.TextDocument,
+  range: vscode.Range | null
+) {
+  const cmd = getCmdName();
+  if (cmd === isNotSupported) {
+    vscode.window.showErrorMessage(
+      "Currently, formatyaya-vscode is only supported on Windows and Linux."
+    );
+    return [];
+  }
+
+  if (range === null) {
+    range = initDocumentRange(document);
+  }
+
+  const settings = vscode.workspace.getConfiguration("formate");
+  const useSpace = settings.get("useSpace", false);
+  const spaceCount = settings.get("spaceCount", 2);
+
+  const sep = __dirname.includes("/") ? "/" : "\\";
+  const result: vscode.TextEdit[] = [];
+  const content = document.getText(range);
+
+  const tmpobj = fileSync({ prefix: "formatyaya-", postfix: ".dic" });
+  writeFileSync(tmpobj.name, content);
+
+  let formatted: Buffer;
+  try {
+    const options: string[] = [];
+    if (useSpace) {
+      options.push("-s");
     }
+    options.push("-c", spaceCount.toString(), tmpobj.name);
 
-	const settings = vscode.workspace.getConfiguration('formate');
-	const useSpace = settings.get('useSpace',false);
-	const spaceCount = settings.get('spaceCount', 2);
+    formatted = execFileSync(`${__dirname}${sep}${cmd}`, options);
+  } catch (error) {
+    throw error;
+  }
 
-	const sep = __dirname.includes("/") ? "/" : "\\";
-	const result: vscode.TextEdit[] = [];
-    const content = document.getText(range);
+  tmpobj.removeCallback();
 
-	const tmpobj = fileSync({ prefix: 'formatyaya-',postfix: '.dic'});
-	writeFileSync(tmpobj.name, content);
+  if (formatted) {
+    result.push(new vscode.TextEdit(range, formatted.toString()));
+  }
 
-	let formatted : Buffer;
-	try {
-		const options :string[] = [];
-		if (useSpace) {
-			options.push("-s");
-		}
-		options.push("-c",spaceCount.toString(),tmpobj.name);
+  return result;
+}
 
-		formatted = execFileSync(`${__dirname}${sep}formatyaya.exe`,options);
-	} catch(error) {
-		throw(error);
-	}
+function getCmdName(): string {
+  let name = "formatyaya_";
+  let ext = "";
+  switch (platform.toString()) {
+    case "win32":
+      name += "windows_";
+      ext = ".exe";
+      break;
+    case "linux":
+      name += "linux_";
+      break;
+    default:
+      console.log(platform.toString());
+      return isNotSupported;
+  }
 
-	if (formatted) {
-		result.push(new vscode.TextEdit(range, formatted.toString()));
-	}
+  switch (arch.toString()) {
+    case "x64":
+      name += "amd64";
+      break;
+    case "arm":
+      name += "arm";
+      break;
+    case "arm64":
+      name += "arm64";
+      break;
+    default:
+      console.log(arch.toString());
+      return isNotSupported;
+  }
 
-    return result;
+  return name + ext;
 }
 
 function initDocumentRange(document: vscode.TextDocument): vscode.Range {
-    const lastLine = document.lineCount - 1;
-    const start = new vscode.Position(0, 0);
-    const end = new vscode.Position(lastLine, document.lineAt(lastLine).text.length);
+  const lastLine = document.lineCount - 1;
+  const start = new vscode.Position(0, 0);
+  const end = new vscode.Position(
+    lastLine,
+    document.lineAt(lastLine).text.length
+  );
 
-    return new vscode.Range(start, end);
+  return new vscode.Range(start, end);
 }
 
 export function deactivate() {}
